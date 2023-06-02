af386e14e968997c536f9a308087eb9f3b34235b
Mohamed <128575094+msayed6789@users.noreply.github.com>
2023-06-02 19:08:17 +0300

    commit no.3

---
+++ SOS/SOS/Debug/main.d
@@ -0,0 +1,58 @@
+main.d main.o: .././main.c .././APP/app.h .././APP/../SERVICE/sos/sos.h \
+ d:\sos_module\sos\sos\std_libraries\std_types.h \
+ d:\sos_module\sos\sos\std_libraries\bit_math.h \
+ d:\sos_module\sos\sos\std_libraries\std_types.h \
+ d:\sos_module\sos\sos\std_libraries\timer_manager\timer_manager.h \
+ d:\sos_module\sos\sos\mcal\timer\timer_types.h \
+ d:\sos_module\sos\sos\std_libraries\std_types.h \
+ d:\sos_module\sos\sos\std_libraries\bit_math.h .././APP/../HAL/led/led.h \
+ d:\sos_module\sos\sos\mcal\dio\dio.h \
+ d:\sos_module\sos\sos\std_libraries\std_types.h \
+ d:\sos_module\sos\sos\std_libraries\bit_math.h \
+ d:\sos_module\sos\sos\mcal\dio\private_regs.h \
+ .././APP/../STD_LIBRARIES/ex_int_manger/ex_int_manger.h \
+ d:\sos_module\sos\sos\mcal\exti\external_interrupt.h \
+ d:\sos_module\sos\sos\std_libraries\std_types.h \
+ d:\sos_module\sos\sos\std_libraries\bit_math.h \
+ d:\sos_module\sos\sos\mcal\dio\dio.h \
+ d:\sos_module\sos\sos\mcal\exti\ext_int_private_reg.h
+
+.././APP/app.h:
+
+.././APP/../SERVICE/sos/sos.h:
+
+d:\sos_module\sos\sos\std_libraries\std_types.h:
+
+d:\sos_module\sos\sos\std_libraries\bit_math.h:
+
+d:\sos_module\sos\sos\std_libraries\std_types.h:
+
+d:\sos_module\sos\sos\std_libraries\timer_manager\timer_manager.h:
+
+d:\sos_module\sos\sos\mcal\timer\timer_types.h:
+
+d:\sos_module\sos\sos\std_libraries\std_types.h:
+
+d:\sos_module\sos\sos\std_libraries\bit_math.h:
+
+.././APP/../HAL/led/led.h:
+
+d:\sos_module\sos\sos\mcal\dio\dio.h:
+
+d:\sos_module\sos\sos\std_libraries\std_types.h:
+
+d:\sos_module\sos\sos\std_libraries\bit_math.h:
+
+d:\sos_module\sos\sos\mcal\dio\private_regs.h:
+
+.././APP/../STD_LIBRARIES/ex_int_manger/ex_int_manger.h:
+
+d:\sos_module\sos\sos\mcal\exti\external_interrupt.h:
+
+d:\sos_module\sos\sos\std_libraries\std_types.h:
+
+d:\sos_module\sos\sos\std_libraries\bit_math.h:
+
+d:\sos_module\sos\sos\mcal\dio\dio.h:
+
+d:\sos_module\sos\sos\mcal\exti\ext_int_private_reg.h:
